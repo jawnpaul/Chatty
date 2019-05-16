de22ce40123fcbde6c9d7c64683494f644959229
John Paul <jpaul5246@gmail.com>
2019-05-16 17:50:08 +0100

    Implemented input validation

--- app/src/main/java/ng/org/knowit/chatty/MainActivity.java
+++ app/src/main/java/ng/org/knowit/chatty/MainActivity.java
@@ -3,12 +3,44 @@ package ng.org.knowit.chatty;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextInputLayout chatIdInputLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        chatIdInputLayout = findViewById(R.id.chatIdTextInputLayout);
+
+        Button chatButton = findViewById(R.id.chatButton);
+
+        chatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean noError = true;
+                String editTextString = chatIdInputLayout.getEditText().getText().toString();
+                if (editTextString.isEmpty()) {
+                    chatIdInputLayout.setError(getResources().getString(R.string.error_string));
+                    noError = false;
+                } else {
+                    chatIdInputLayout.setError(null);
+                }
+
+                if (noError) {
+                    // All fields are valid!
+                    Toast.makeText(MainActivity.this, "Not well", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
+
     }
 }
