2da8be422cd9c2913437024844c786281505b78d
John Paul <jpaul5246@gmail.com>
2019-05-16 16:15:18 +0100

    First commit

---
+++ app/src/main/java/ng/org/knowit/chatty/MainActivity.java
@@ -0,0 +1,14 @@
+package ng.org.knowit.chatty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
